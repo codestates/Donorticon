050d3ef5fa2bff20dd16bd90103e42e9627a0c21
tiahwang44 <tiahwang44@gmail.com>
2022-02-10 16:29:14 +0900

    CREATE: globalStyle/defaultTheme

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  basicWidth: '100vw',
+  color: {
+    main: '#FFCE44',
+    mainDark: '#E5B93D',
+    error: '#B71C1C',
+  },
+};
+
+export { theme };
