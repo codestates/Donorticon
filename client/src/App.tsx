9c7027beb1c0f71bc35a6865e656faf6ae048838
tiahwang44 <tiahwang44@gmail.com>
2022-02-10 16:29:14 +0900

    CREATE: globalStyle/defaultTheme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return <div>hello world</div>
-}
+const TestDiv = styled.div`
+  color: ${({ theme }) => theme.color.error};
+  font-size: 50px;
+`;
+
+const TestButton = styled.button``;
+
+const App = () => {
+  return (
+    <>
+      <TestDiv>hello world</TestDiv>
+      <TestButton>button</TestButton>
+    </>
+  );
+};
 
 export default App;
