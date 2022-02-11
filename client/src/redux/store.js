7d043ba23f1eabd4b2987577a395f44f529f2d09
Tia Hwang <90431155+tiatiahwang@users.noreply.github.com>
2022-02-11 16:19:11 +0900

    ADD: redux-persist (#122)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
 import { userSlice } from './user/userSlice';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+
+const reducers = combineReducers({
+  user: userSlice.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-  },
+  reducer: persistedReducer,
+  // devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
 
 export default store;
