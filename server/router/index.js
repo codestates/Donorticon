b18efb469f7eeaa56025e91a3b09ccc8fe432456
seanswim <swim1720@gmail.com>
2022-02-14 12:24:07 +0900

    FIX: email verification redux and seqeulize requests

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verification } = require('../controller');
+const { verification } = require('../controller/verification');
 
 router.get('/verification', verification.get);
 router.put('/verification', verification.put);
