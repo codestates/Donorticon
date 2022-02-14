1ba47e58e01ab230f6177f18b02628e7518719ce
tiahwang44 <tiahwang44@gmail.com>
2022-02-14 16:28:11 +0900

    CREATE: Loader

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,9 @@ import {
 } from '../redux/util/utilSlice';
 import Modal from '../component/Modal';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { isAllOf } from '@reduxjs/toolkit';
+import Loader from '../component/Loader';
 
 // 아래 styled 먹인 것들은 추후 수정예정
 // 임시로 기능 확인 위해서 설정해 놓은 것
@@ -34,6 +37,9 @@ const Div = styled.div`
 `;
 
 const Home = () => {
+  // isLoading 값 일단은 수동으로 true/false 값 관리
+  const [isLoading, setIsLoading] = useState(false);
+
   const userState = useSelector(userSelector);
   const utilState = useSelector(utilSelector);
   const dispatch = useDispatch();
@@ -56,34 +62,40 @@ const Home = () => {
   };
 
   return (
-    <Container>
-      <Wrapper>
-        <Button onClick={handleSignInButton}>
-          {userState.isLoggedIn ? '로그아웃' : '로그인'}
-        </Button>
-        {userState.isLoggedIn ? (
-          <Link to="/mypage">
-            <Button>마이페이지</Button>
-          </Link>
-        ) : (
-          <Button onClick={handleSignUpButton}>회원가입</Button>
-        )}
-        <Modal
-          content={'hi'}
-          buttonList={['Giver', 'Helper']}
-          nextPage={utilState.nextPage}
-          buttonEndPoint={utilState.nextPage === '/signup' ? true : false}
-          callback={setWho}
-        />
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-        <Div>TEST</Div>
-      </Wrapper>
-    </Container>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Container>
+          <Wrapper>
+            <Button onClick={handleSignInButton}>
+              {userState.isLoggedIn ? '로그아웃' : '로그인'}
+            </Button>
+            {userState.isLoggedIn ? (
+              <Link to="/mypage">
+                <Button>마이페이지</Button>
+              </Link>
+            ) : (
+              <Button onClick={handleSignUpButton}>회원가입</Button>
+            )}
+            <Modal
+              content={'hi'}
+              buttonList={['Giver', 'Helper']}
+              nextPage={utilState.nextPage}
+              buttonEndPoint={utilState.nextPage === '/signup' ? true : false}
+              callback={setWho}
+            />
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+            <Div>TEST</Div>
+          </Wrapper>
+        </Container>
+      )}
+    </>
   );
 };
 
---
+++ client/src/styles/LoaderStyle.js
@@ -0,0 +1,44 @@
+import styled, { keyframes } from 'styled-components';
+
+export const LoaderContainer = styled.div`
+  height: calc(100vh - 100px);
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+`;
+
+const Animate = keyframes`
+    0% {
+      transform: scale(0, 0);opacity:0;
+    }
+    100% {
+      transform: scale(1, 1);opacity:1;
+    }
+`;
+
+export const Box1 = styled(Box)`
+  background: rgba(255, 206, 68, 0.6);
+  animation: ${Animate} 1s ease-in-out infinite alternate;
+`;
+
+export const Box2 = styled(Box)`
+  background: rgba(255, 206, 68, 0.4);
+  animation: ${Animate} 1.8s ease-in-out infinite alternate;
+`;
+
+export const Box3 = styled(Box)`
+  background: rgba(255, 206, 68, 0.3);
+  animation: ${Animate} 1.1s ease-in-out infinite alternate;
+`;
+
+export const Box4 = styled(Box)`
+  background: rgba(255, 206, 68, 0.5);
+  animation: ${Animate} 1.4s ease-in-out infinite alternate;
+`;
