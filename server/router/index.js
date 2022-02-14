fc8686c6c8b1a2a98b4fae84fa481a7d94f14b8c
seanswim <swim1720@gmail.com>
2022-02-14 17:59:06 +0900

    FIX: merge conflict - gifticon page

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { verification } = require('../controller/verification');
+const { gifticon } = require('../controller/gifticon')
 
 router.get('/verification', verification.get);
 router.put('/verification', verification.put);
+router.get('/gifticon', gifticon.get);
 
 module.exports = router;
