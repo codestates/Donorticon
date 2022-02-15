14c2dc6a596a51e609d072f287120b3d5225ce74
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-02-15 17:07:09 +0900

    CREATE: helper sign up checklist

--- client/src/pages/SignUp/SignUpHelper.js
+++ client/src/pages/SignUp/SignUpHelper.js
@@ -12,13 +12,35 @@ import {
   Label,
   CheckList,
 } from '../../styles/SignUpStyle';
+import sha256 from 'js-sha256';
+import axios from 'axios';
 
 const SignUpHelper = () => {
+  const [helperInfo, setHeperInfo] = useState({
+    vulunable: [],
+    gifticon_category: [],
+    location: '',
+    email: '',
+    name: '',
+    password: '',
+    mobile: '',
+  });
+
+  const [isValid, setIsValid] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+  ]);
   const [percent, setPercent] = useState(0);
   const [page, setPage] = useState(0);
   const signUpForm = [
     {
       contentGuide: '어떤 분들을 돕고 계신가요?',
+      name: 'vulunable',
       lists: [
         '아동청소년',
         '어르신',
@@ -31,6 +53,7 @@ const SignUpHelper = () => {
     },
     {
       contentGuide: '무엇을 지원 받고 싶으신가요?',
+      name: 'gifticon_category',
       lists: [
         '식품',
         '화장품',
@@ -47,9 +70,94 @@ const SignUpHelper = () => {
     },
     {
       contentGuide: '마지막으로 필수 정보를 입력해 주세요',
-      list: [],
+      // input: [
+      //   {
+      //     title: '이메일',
+      //     inputPlaceHolder: '이메일을 입력해주세요',
+      //     callback: (e) => {
+      //       setGiverInfo(Object.assign(giverInfo, { email: e.target.value }));
+      //       const form = new RegExp(
+      //         '^[0-9a-zA-Z._%+-]+@[0-9a-zA-Z.-]+\\.[a-zA-Z]{2,6}$',
+      //       );
+      //       const validList = [...isValid];
+      //       validList[0] = form.test(e.target.value);
+      //       setIsValid(validList);
+      //       return !form.test(e.target.value);
+      //     },
+      //     errorMessage: '이메일 형식이 맞지 않습니다',
+      //   },
+      //   {
+      //     title: '이름',
+      //     inputPlaceHolder: '8자 이내로 입력해주세요',
+      //     callback: (e) => {
+      //       setGiverInfo(Object.assign(giverInfo, { name: e.target.value }));
+      //       const validList = [...isValid];
+      //       validList[1] = e.target.value.length <= 8;
+      //       setIsValid(validList);
+      //       return !(e.target.value.length <= 8);
+      //     },
+      //     errorMessage: '8자 이상의 이름입니다',
+      //   },
+      //   {
+      //     title: '비밀번호',
+      //     inputPlaceHolder: '비밀번호를 입력해주세요',
+      //     callback: (e) => {
+      //       setGiverInfo(
+      //         Object.assign(giverInfo, { password: sha256(e.target.value) }),
+      //       );
+      //       const validList = [...isValid];
+      //       validList[2] = e.target.value.length >= 1;
+      //       setIsValid(validList);
+      //       return !(e.target.value.length >= 1);
+      //     },
+      //     errorMessage: '비밀번호를 입력해주세요',
+      //   },
+      //   {
+      //     title: '비밀번호 확인',
+      //     inputPlaceHolder: '비밀번호를 확인해주세요',
+      //     callback: (e) => {
+      //       const validList = [...isValid];
+      //       validList[3] = sha256(e.target.value) === giverInfo.password;
+      //       setIsValid(validList);
+      //       return sha256(e.target.value) !== giverInfo.password;
+      //     },
+      //     errorMessage: '비밀번호가 일치하지 않습니다',
+      //   },
+      //   {
+      //     title: '휴대전화',
+      //     inputPlaceHolder: '010-0000-0000 형식으로 입력해주세요',
+      //     callback: (e) => {
+      //       setGiverInfo(Object.assign(giverInfo, { mobile: e.target.value }));
+      //       const form = new RegExp('^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$');
+      //       const validList = [...isValid];
+      //       validList[4] = e.target.value ? form.test(e.target.value) : true;
+      //       setIsValid(validList);
+      //       return e.target.value ? !form.test(e.target.value) : false;
+      //     },
+      //     errorMessage: '전화번호 형식이 맞지 않습니다',
+      //   },
+      // ],
     },
   ];
+
+  const handleCheckBox = (e) => {
+    if (e.target.checked) {
+      setHeperInfo(
+        Object.assign(helperInfo, {
+          [e.target.name]: [...helperInfo[e.target.name], e.target.value],
+        }),
+      );
+    } else {
+      setHeperInfo(
+        Object.assign(helperInfo, {
+          [e.target.name]: [...helperInfo[e.target.name]].filter(
+            (list) => list !== e.target.value,
+          ),
+        }),
+      );
+    }
+  };
+
   const handleButton = (e) => {
     if (e.target.textContent === '다음') {
       if (page <= 2) {
@@ -63,9 +171,13 @@ const SignUpHelper = () => {
       }
     }
   };
+
   useEffect(() => {
     setPercent(percent + 25);
   }, []);
+
+  useEffect(() => {});
+
   return (
     <Container>
       <ContentBox>
@@ -73,9 +185,17 @@ const SignUpHelper = () => {
         <ProgressBar percent={percent} />
         <ContentGuider>{signUpForm[page].contentGuide}</ContentGuider>
         {signUpForm[page].lists
-          ? signUpForm[page].lists.map((list, idx, arr) => (
-              <CheckList key={idx}>
-                <Input value={list} type={'checkbox'} />
+          ? signUpForm[page].lists.map((list, idx) => (
+              <CheckList key={page * 7 + idx}>
+                <Input
+                  name={signUpForm[page].name}
+                  value={list}
+                  type={'checkbox'}
+                  onClick={handleCheckBox}
+                  defaultChecked={helperInfo[signUpForm[page].name].includes(
+                    list,
+                  )}
+                />
                 <Label>{list}</Label>
               </CheckList>
             ))
