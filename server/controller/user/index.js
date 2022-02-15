f6f82a44fb65cd970be6b6e5d16f584a91e62554
Mubarmig <throughworld97@gmail.com>
2022-02-15 22:20:20 +0900

    CREATE: signup-helper, signin-giver, signin-helper

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -1,5 +1,9 @@
 module.exports = {
   signup_giver: require('./signup_giver'),
+  signup_helper: require('./signup_helper'),
+  signin_giver: require('./signin_giver'),
+  signin_helper: require('./signin_helper'),
+
   googleLogin: require('./googleLogin'),
   kakaoLogin: require('./kakaoLogin'),
   signout: require('./signout'),
--- server/index.js
+++ server/index.js
@@ -24,9 +24,9 @@ app.use('/', router);
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
 let server;
-if (fs.existsSync('../key.pem') && fs.existsSync('../cert.pem')) {
-  const privateKey = fs.readFileSync(__dirname + '/../key.pem', 'utf8');
-  const certificate = fs.readFileSync(__dirname + '/../cert.pem', 'utf8');
+if (fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')) {
+  const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+  const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
   const credentials = { key: privateKey, cert: certificate };
 
   server = https.createServer(credentials, app);
