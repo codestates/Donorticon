e15a7ed76b82f361c2a6ce75143249ffcae70e3b
tiahwang44 <tiahwang44@gmail.com>
2022-02-16 15:11:46 +0900

    CREATE: helperList

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   user: require('./user'),
-  verification: require( './verification' )
-}
+  verification: require('./verification'),
+  helperlist: require('./helperlist'),
+};
