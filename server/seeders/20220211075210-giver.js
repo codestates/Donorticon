05a018677e8e32d4c229c5be70edd9bf545befb1
Mubarmig <throughworld97@gmail.com>
2022-02-17 14:45:12 +0900

    FIX: signup-helper

--- server/seeders/20220211075210-giver.js
+++ server/seeders/20220211075210-giver.js
@@ -8,7 +8,10 @@ module.exports = {
       email: 'kimcoding@codestates.com',
       password: '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
       mobile: '010-1234-5678',
-      img: 'https://drive.google.com/file/d/1ZZ623uRSdNtvDhcho27A5-SjdIdn2Daw/view?usp=sharing',
+      img: `https://randomuser.me/api/portraits/women/${getRandomNumber(
+        1,
+        98,
+      )}.jpg`,
       black_point: 0,
       createdAt: new Date(),
       updatedAt: new Date()
--- server/seeders/20220211095403-gallery.js
+++ server/seeders/20220211095403-gallery.js
@@ -2,12 +2,14 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('galleries', [{
+    await queryInterface.bulkInsert('galleries', [
+      {
       helper_id: 1,
       img: 'https://drive.google.com/file/d/1R0rY7gKJTVz0bDbaIN3alL_Or-hsk-J5/view?usp=sharing', 
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});  
+      },
+    ], {});  
   },
 
   async down (queryInterface, Sequelize) {
