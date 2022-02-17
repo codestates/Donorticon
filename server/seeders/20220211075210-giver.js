89b593db0b9669d982c0f24935457be7cb5cce37
tiahwang44 <tiahwang44@gmail.com>
2022-02-17 16:16:04 +0900

    UPDATE: googleLogin if email already exists

--- server/seeders/20220211075210-giver.js
+++ server/seeders/20220211075210-giver.js
@@ -1,23 +1,35 @@
 'use strict';
 
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('givers', [{
-      user_type: 1,
-      name: 'kimcoding',
-      email: 'kimcoding@codestates.com',
-      password: '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
-      mobile: '010-1234-5678',
-      img: 'https://drive.google.com/file/d/1ZZ623uRSdNtvDhcho27A5-SjdIdn2Daw/view?usp=sharing',
-      black_point: 0,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});  
-  },
-
-  async down (queryInterface, Sequelize) {
-
-    await queryInterface.bulkDelete('givers', null, {});
-  }
+const getRandomNumber = (min, max) => {
+  return parseInt(Math.random() * (Number(max) - Number(min) + 2));
 };
 
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'givers',
+      [
+        {
+          user_type: 1,
+          name: 'kimcoding',
+          email: 'kimcoding@codestates.com',
+          password:
+            '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4',
+          mobile: '010-1234-5678',
+          img: `https://randomuser.me/api/portraits/women/${getRandomNumber(
+            1,
+            98,
+          )}.jpg`,
+          black_point: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('givers', null, {});
+  },
+};
