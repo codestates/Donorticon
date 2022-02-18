43d4e3b642313d5fef31e9cd86a3c1c0492adf2e
Mubarmig <87626133+Mubarmig@users.noreply.github.com>
2022-02-18 12:40:31 +0900

    [server] guest signin (수정)
    머지했는데 적용이 안되서 수동으로 수정합니다

--- server/controller/user/index.js
+++ server/controller/user/index.js
@@ -3,6 +3,8 @@ module.exports = {
   signup_helper: require('./signup_helper'),
   signin_giver: require('./signin_giver'),
   signin_helper: require('./signin_helper'),
+  signin_giver_guest: require('./signin_giver_guest'),
+  signin_helper_guest: require('./signin_helper_guest'),
 
   googleLogin: require('./googleLogin'),
   kakaoLogin: require('./kakaoLogin'),
