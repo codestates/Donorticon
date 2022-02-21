4f990b1e3b825bb5aaaca5607a0a2f8a84c464b0
Mubarmig <throughworld97@gmail.com>
2022-02-21 16:18:00 +0900

    CREATE: mypage modification

--- server/controller/user/mypageHelper.js
+++ server/controller/user/mypageHelper.js
@@ -1,30 +1,31 @@
 const jwt_decode = require('jwt-decode');
 const { helper, helper_vulnerable, helper_gifticon_category } = require('../../models');
 
-module.exports = async (req, res) => {
-	try {
-		const token = req.headers.token;
-		const tokenDecoded = jwt_decode(token);
-		const { email, name, mobile, slogan, description, location } = tokenDecoded;
-		const helperRow = await helper.findOne({
-			where: { email }
-		});
-		const helperId = helperRow.id;
-		const helper_vulnerableRow = await helper_vulnerable.findAll({
-			where: { helper_id: helperId }
-		});
-		const helper_gifticon_categoryRow = await helper_gifticon_category.findAll({
-			where: { helper_id: helperId }
-		});
-		const vulnerableList = helper_vulnerableRow.map(el => Number(el.dataValues.vulnerable_id));
-		const gifticonCategoryList =  helper_gifticon_categoryRow.map(el => Number(el.dataValues.gifticon_category_id));
-		
-		// console.log({ '취약계층': vulnerableList, '기프티콘카테고리': gifticonCategoryList })
-		const data = { email, name, mobile, slogan, description, location, vulnerable: vulnerableList, gifticonCategory: gifticonCategoryList, gallery: ['사진추가예정'] }
-		console.log(data);
-		res.status(200).json(data);
-	} catch(err) {
-		console.log(err);
+module.exports = {
+	get: async (req, res) => {
+		try {
+			const token = req.headers.token;
+			const tokenDecoded = jwt_decode(token);
+			const { id, email, name, mobile, slogan, description, location, img } = tokenDecoded;
+			const helperRow = await helper.findOne({
+				where: { email }
+			});
+			const helperId = helperRow.id;
+			const helper_vulnerableRow = await helper_vulnerable.findAll({
+				where: { helper_id: helperId }
+			});
+			const helper_gifticon_categoryRow = await helper_gifticon_category.findAll({
+				where: { helper_id: helperId }
+			});
+			const vulnerableList = helper_vulnerableRow.map(el => Number(el.dataValues.vulnerable_id));
+			const gifticonCategoryList =  helper_gifticon_categoryRow.map(el => Number(el.dataValues.gifticon_category_id));
+			
+			const data = { id, email, name, mobile, slogan, description, location, vulnerable: vulnerableList, gifticonCategory: gifticonCategoryList, gallery: ['사진추가예정'], img }
+			
+			res.status(200).json(data);
+		} catch(err) {
+			console.log(err);
+		}
 	}
 }
 
