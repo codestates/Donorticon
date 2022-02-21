bc0da855337faad4c6a04ad79f0317cfb788d339
tiahwang44 <tiahwang44@gmail.com>
2022-02-21 22:36:23 +0900

    UPDATE: giver point level

--- client/src/component/GiverLevel.js
+++ client/src/component/GiverLevel.js
@@ -12,48 +12,46 @@ import platinum from '../img/pointGrade/3_platinum.png';
 import diamond from '../img/pointGrade/4_diamond.png';
 import { useEffect, useState } from 'react';
 
-const pointGrade = [
-  { id: 0, name: '브론즈 연탄', src: bronze },
-  { id: 1, name: '실버 연탄', src: silver },
-  { id: 2, name: '골드 연탄', src: gold },
-  { id: 3, name: '플래티넘 연탄', src: platinum },
-  { id: 4, name: '다이아몬드 연탄', src: diamond },
-];
+const GiverLevel = ({ point }) => {
+  const [level, setLevel] = useState('');
 
-const GiverLevel = ({ grade }) => {
-  const [level, setLevel] = useState();
-  console.log('grade', grade);
-  console.log('first', level);
-
-  const getLevel = (grade) => {
-    if (grade !== 0) {
-      const filtered = pointGrade.filter((x) => x.id === grade);
-      setLevel(filtered[0]);
-      console.log('second', level);
+  const getLevel = (point) => {
+    if (point !== 0) {
+      if (point >= 21) {
+        setLevel({ id: 4, name: '다이아몬드 연탄', src: diamond });
+      } else if (point >= 16) {
+        setLevel({ id: 3, name: '플래티넘 연탄', src: platinum });
+      } else if (point >= 11) {
+        setLevel({ id: 2, name: '골드 연탄', src: gold });
+      } else if (point >= 6) {
+        setLevel({ id: 1, name: '실버 연탄', src: silver });
+      } else if (point >= 0) {
+        setLevel({ id: 0, name: '브론즈 연탄', src: bronze });
+      }
     } else {
       setLevel({ id: 0, name: '브론즈 연탄', src: bronze });
     }
   };
 
-  useEffect(() => getLevel(grade), [grade]);
+  useEffect(() => getLevel(point), [point]);
 
   return (
     <>
       <LevelContainer>
         <Title>GIVER</Title>
       </LevelContainer>
-      {level && (
+      {level && point && (
         <LevelContents>
           <LevelImage src={level.src} />
           <LevelName>{level.name}</LevelName>
         </LevelContents>
       )}
-      {grade === undefined && (
+      {/* {point === 0 && (
         <LevelContents>
           <LevelImage src={bronze} />
           <LevelName>브론즈 연탄</LevelName>
         </LevelContents>
-      )}
+      )} */}
     </>
   );
 };
