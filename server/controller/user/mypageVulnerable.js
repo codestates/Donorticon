2b20d163a9df342d1364a90377aeb71366026f73
Jin-hyeok_Heo <hwlsgur1120@naver.com>
2022-02-22 17:16:25 +0900

    CREATE: editmyInfo

--- server/controller/user/mypageVulnerable.js
+++ server/controller/user/mypageVulnerable.js
@@ -3,36 +3,39 @@ const jwt_decode = require('jwt-decode');
 const { helper_vulnerable } = require('../../models');
 
 module.exports = {
-	post: async (req, res) => {
-		try {
-			const vulnerableId = req.body.vulnerable_id;
-			const { token } = req.headers;
-			const tokenDecoded = jwt_decode(token);
-			const helperId = tokenDecoded.id;
-			await helper_vulnerable.create({
-				helper_id: helperId,
-				vulnerable_id: vulnerableId
-			});
-			res.status(201).json({ message: "data successfully added"});
-		} catch(err) {
-			console.log(err);
-		}
-	},
-	delete: async (req, res) => {
-		try {
-			console.log('파라미터', req.query);
-			const vulnerableId = req.body.vulnerable_id;
-			const { token } = req.headers;
-			const tokenDecoded = jwt_decode(token);
-			const helperId = tokenDecoded.id;
-			console.log(tokenDecoded);
-			await helper_vulnerable.destroy({
-				helper_id: helperId,
-				vulnerable_id: vulnerableId
-			});
-			res.json({ message: "data successfully deleted"});
-		} catch(err) {
-			console.log(err);
-		}
-	}
-}
+  post: async (req, res) => {
+    try {
+      const vulnerableId = req.body.vulnerable_id;
+      const { token } = req.headers;
+      const tokenDecoded = jwt_decode(token);
+      const helperId = tokenDecoded.id;
+      await helper_vulnerable.create({
+        helper_id: helperId,
+        vulnerable_id: vulnerableId,
+      });
+      res.status(201).json({ message: 'data successfully added' });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  delete: async (req, res) => {
+    console.log('삭제 작동중');
+    try {
+      console.log('파라미터', req.query);
+      const vulnerableId = req.query.vulnerable_id;
+      const { token } = req.headers;
+      const tokenDecoded = jwt_decode(token);
+      const helperId = tokenDecoded.id;
+      console.log(tokenDecoded);
+      await helper_vulnerable.destroy({
+        where: {
+          helper_id: helperId,
+          vulnerable_id: vulnerableId,
+        },
+      });
+      res.json({ message: 'data successfully deleted' });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
