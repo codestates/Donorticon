99244e48c4a739055e3169b1587f799c32d52286
Mubarmig <throughworld97@gmail.com>
2022-02-24 20:06:25 +0900

    a

--- server/controller/index.js
+++ server/controller/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   user: require('./user'),
+  mypage: require('./mypage'),
   verification: require('./verification'),
   helperList: require('./helperList'),
   gifticon: require('./gifticon'),
