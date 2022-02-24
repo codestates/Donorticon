93ad91212693797cc591f885d3eb3ca1101c58f7
Mubarmig <throughworld97@gmail.com>
2022-02-24 12:27:21 +0900

    CREATE: helper mypage activity

--- server/controller/user/mypageHelperActivity.js
+++ server/controller/user/mypageHelperActivity.js
@@ -1,7 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { helper } = require('../../models');
+
 module.exports = {
 	put: async (req, res) => {
 		try {
-			console.log(req.body, req.query, req.headers);
+			const token = req.headers.token;
+			const tokenDecoded = jwt.verify(token, process.env.ACCESS_SECRET);
+			const { id } = tokenDecoded;
+			if (typeof req.body.activity === 'boolean') {
+				await helper.update(
+					{	activity: req.body.activity },
+					{
+						where: { id }
+					}
+				)
+			}
+			console.log(tokenDecoded);
 		} catch(err) {
 			console.log(err);
 		}
