7853521e7084b0a4310c21176e409af92feb9b03
tiahwang44 <tiahwang44@gmail.com>
2022-02-27 08:57:38 +0900

    ADD: mypage CSS

--- client/src/styles/Mypage/MypageStyle.js
+++ client/src/styles/Mypage/MypageStyle.js
@@ -2,15 +2,25 @@ import styled from 'styled-components';
 
 export const ContentLeft = styled.div`
   width: 70%;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100%;
+  }
 `;
 
 export const ContentRight = styled.div`
   width: 30%;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100%;
+    padding-bottom: 20px;
+  }
 `;
 
 export const InputBox = styled.div`
   padding: 10px 0;
   text-align: left;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100%;
+  }
 `;
 
 export const InputName = styled.div`
@@ -18,45 +28,76 @@ export const InputName = styled.div`
   text-align: left;
 `;
 
-export const InputContent = styled.div`
-  width: 80%;
-  padding: 10px 0;
-  margin: 10px 0;
-  border-bottom: 1px solid ${({ theme }) => theme.color.lightGrey};
-`;
-
 export const InputChanger = styled.input`
   all: unset;
   width: 80%;
   padding: 10px 0;
   margin: 10px 0;
-  width: 80%;
   border-bottom: 1px solid ${({ theme }) => theme.color.main};
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100%;
+  }
+`;
+
+export const InputContent = styled.div`
+  width: 80%;
+  padding: 10px 0;
+  margin: 10px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.color.lightGrey};
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100%;
+  }
 `;
 
 export const MultiContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 10px 0;
+  width: ${(props) => props.gallery && '80%'};
+  align-items: ${(props) => props.center && 'center'};
+  @media ${({ theme }) => theme.device.mobile} {
+    width: ${(props) => props.gallery && '100%'};
+  }
 `;
 
 export const GalleryBox = styled.div`
-  width: 20%;
+  width: 100%;
   height: 100px;
   margin: 10px 0;
+  display: flex;
 `;
 
 export const GalleryImg = styled.img`
-  height: 100px;
+  width: 20%;
+  height: 100%;
+  border: 1px solid ${({ theme }) => theme.color.lightGrey};
 `;
 
-export const ProfileImg = styled.img`
-  width: 70%;
+export const ActBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+
+export const ActButton = styled.button`
+  margin: 10px 0;
+  cursor: pointer;
+  width: 90px;
+  &:hover {
+    color: ${({ theme }) => theme.color.main};
+  }
+`;
+
+export const ProfileImg = styled.div`
   border-radius: 50%;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  width: 150px;
+  height: 150px;
 `;
 
 export const GalleryAddLabel = styled.label`
-  width: 100%;
+  width: ${(props) => (props.center ? '150px' : '100%')};
   border: 1px solid ${({ theme }) => theme.color.main};
   background-color: #fff;
   padding: 10px 0;
@@ -68,10 +109,4 @@ export const GalleryAddLabel = styled.label`
 
 export const NotShow = styled.input`
   display: none;
-`;
-
-export const ActButton = styled.button`
-  color: ${({ theme }) => theme.color.lightGrey};
-  margin: 5px;
-  cursor: pointer;
 `;
