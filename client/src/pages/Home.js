f2a7c295219a769b9b18488b16b805825d83e258
tiahwang44 <tiahwang44@gmail.com>
2022-03-01 23:40:09 +0900

    ADD: landing page 01 CSS

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Banner from '../component/Landing/Banner';
 import HowToUse from '../component/Landing/HowToUse';
-import Helpers from '../component/Landing/Helpers';
+
 import Reviews from '../component/Landing/Reviews';
 import Cases from '../component/Landing/Cases';
 import FloatingButton from '../component/Landing/FloatingButton';
@@ -11,36 +10,15 @@ const Container = styled.div`
   margin-top: -75px; // 이 값은 HEADER 값과 동일해야함!!
 `;
 
-const Wrapper = styled.div``;
-
-const Div = styled.div`
-  font-size: 5rem;
-`;
-
 const Home = () => {
   return (
-    <>
-      <Container>
-        <Wrapper>
-          <Div>
-            <Banner />
-          </Div>
-          <Div>
-            <HowToUse />
-          </Div>
-          <Div>
-            <Helpers />
-          </Div>
-          <Div>
-            <Reviews />
-          </Div>
-          <Div>
-            <Cases />
-          </Div>
-        </Wrapper>
-        <FloatingButton />
-      </Container>
-    </>
+    <Container>
+      <Banner />
+      <HowToUse />
+      <Reviews />
+      <Cases />
+      <FloatingButton />
+    </Container>
   );
 };
 
--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -3,12 +3,13 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
+    @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;&display=swap');
     * {
         box-sizing: border-box;
     }
     html,body {
         font-size: 18px;
-        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, 'baloo';
         @media (max-width: 414px) {
             font-size: 15px;
          }
