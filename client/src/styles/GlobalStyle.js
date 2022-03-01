92a99430960d25f43c5d804374a0244e100e3ca6
tiahwang44 <tiahwang44@gmail.com>
2022-03-02 00:29:45 +0900

    FIX: font

--- client/src/styles/GlobalStyle.js
+++ client/src/styles/GlobalStyle.js
@@ -3,13 +3,13 @@ import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
     ${reset}
-    @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Baloo+2:wght@500;800&display=swap');
     * {
         box-sizing: border-box;
     }
     html,body {
         font-size: 18px;
-        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, 'baloo';
+        font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         @media (max-width: 414px) {
             font-size: 15px;
          }
