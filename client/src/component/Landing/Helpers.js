3fcda39e48564dcddaf2685567966a29ee3f78ef
tiahwang44 <tiahwang44@gmail.com>
2022-03-02 01:22:22 +0900

    ADD: landing page 02 03 04 CSS

---
+++ client/src/component/Landing/Helpers.js
@@ -0,0 +1,16 @@
+import { LandingContainer, TextBox } from '../../styles/Landing/CommonStyle';
+
+const Helpers = () => {
+  return (
+    <LandingContainer>
+      <TextBox>
+        많은 Helper들이 Donorticon과
+        <br />
+        함께 하고 있습니다
+      </TextBox>
+      <div>헬퍼 이미지들</div>
+    </LandingContainer>
+  );
+};
+
+export default Helpers;
