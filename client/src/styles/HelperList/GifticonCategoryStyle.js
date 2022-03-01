b16c9617748e16f48882ec1bc1b71d35c82918f8
tiahwang44 <tiahwang44@gmail.com>
2022-03-01 13:37:45 +0900

    ADD: helper list mobile CSS

--- client/src/styles/HelperList/GifticonCategoryStyle.js
+++ client/src/styles/HelperList/GifticonCategoryStyle.js
@@ -9,11 +9,20 @@ export const GifticonCategoryContainer = styled.div`
 `;
 
 export const GifticonCategoryBox = styled.div`
+  width: 90%;
   display: flex;
-
+  justify-content: center;
+  cursor: pointer;
   @media ${({ theme }) => theme.device.tablet} {
-    width: 100%;
-    padding: 0 40px;
+    overflow-x: scroll;
+    display: inline-block;
+    white-space: nowrap;
+    // 스크롤바 숨기기
+    &::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
+    }
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
   }
 `;
 
@@ -21,13 +30,13 @@ export const GifticonContent = styled.div`
   border: 1px solid #000;
   border-radius: 10px;
   padding: 5px 10px;
-  margin-right: 15px;
+  margin: 0 5px;
   cursor: pointer;
   &.active {
     background-color: ${({ theme }) => theme.color.main};
     border: 2px solid ${({ theme }) => theme.color.main};
   }
   @media ${({ theme }) => theme.device.tablet} {
-    white-space: nowrap;
+    display: inline-block;
   }
 `;
